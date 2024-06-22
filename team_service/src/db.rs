c41dce29a583487207e1b8cdd0a2773fa510a440
Marcelo Fabiano <marcelofabianov@gmail.com>
2024-06-22 17:45:20 -0300

    chore: criar conexao com banco postgres

---
+++ team_service/src/db.rs
@@ -0,0 +1,15 @@
+use dotenv::dotenv;
+use sqlx::{postgres::PgPoolOptions, PgPool};
+use std::env;
+
+pub async fn connect() -> Result<PgPool, sqlx::Error> {
+    dotenv().ok();
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&database_url)
+        .await?;
+
+    Ok(pool)
+}
